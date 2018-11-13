e042e3feaadcd37abcb449adaaee41e16e1dfae4
Allan Sandfeld Jensen <allan.jensen@qt.io>
2018-11-13 17:14:43 +0100

    Fix -Wdeprecated-copy warnings
    Implicit copy constructors or methods are considered deprecated for
    classes that has one of the two or a destructor.
    
    The warning is enabled with -Wextra in gcc 9
    
    Change-Id: Ic9be654f2a142fb186a4d5a7d6b4f7d6f4e611d8
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    
    Upstream-Status: Backport from 5.13
    
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

--- src/corelib/tools/qlist.h
+++ src/corelib/tools/qlist.h
@@ -72,7 +72,7 @@ template <typename T> class QSet;
 template <typename T> struct QListSpecialMethods
 {
 protected:
-    ~QListSpecialMethods() {}
+    ~QListSpecialMethods() = default;
 };
 template <> struct QListSpecialMethods<QByteArray>;
 template <> struct QListSpecialMethods<QString>;
@@ -237,6 +237,8 @@ public:
         // can't remove it in Qt 5, since doing so would make the type trivial,
         // which changes the way it's passed to functions by value.
         inline iterator(const iterator &o) Q_DECL_NOTHROW : i(o.i){}
+        inline iterator &operator=(const iterator &o) Q_DECL_NOTHROW
+        { i = o.i; return *this; }
 #endif
         inline T &operator*() const { return i->t(); }
         inline T *operator->() const { return &i->t(); }
@@ -290,6 +292,8 @@ public:
         // can't remove it in Qt 5, since doing so would make the type trivial,
         // which changes the way it's passed to functions by value.
         inline const_iterator(const const_iterator &o) Q_DECL_NOTHROW : i(o.i) {}
+        inline const_iterator &operator=(const const_iterator &o) Q_DECL_NOTHROW
+        { i = o.i; return *this; }
 #endif
 #ifdef QT_STRICT_ITERATORS
         inline explicit const_iterator(const iterator &o) Q_DECL_NOTHROW : i(o.i) {}
