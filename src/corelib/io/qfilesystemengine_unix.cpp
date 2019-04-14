37ea0b0c40f541cf9741c064e97ec9e9327ba7a1
Andreas Müller <schnitzeltony@gmail.com>
2019-04-14 13:27:58 +0200

    Avoid renameeat2 for native(sdk) builds
    Recently pseudo changed to not support reanameeat2 as glibc wrapper [1]. This
    causes massive failures at do_install [2] on qtbase.
    
    To work around tell Qt build configuration not to use ranameet2 independent
    of glibc version.
    
    [1] https://git.openembedded.org/openembedded-core/commit/?id=0fb257121b68f38b40c078150db8f7d0979b7ea5
    [2] https://github.com/meta-qt5/meta-qt5/issues/187
    
    Upstream-Status: Inappropriate [OE-specific]
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/corelib/io/qfilesystemengine_unix.cpp
+++ src/corelib/io/qfilesystemengine_unix.cpp
@@ -1223,16 +1223,6 @@ bool QFileSystemEngine::renameFile(const QFileSystemEntry &source, const QFileSy
     Q_CHECK_FILE_NAME(srcPath, false);
     Q_CHECK_FILE_NAME(tgtPath, false);
 
-#if defined(RENAME_NOREPLACE) && QT_CONFIG(renameat2)
-    if (renameat2(AT_FDCWD, srcPath, AT_FDCWD, tgtPath, RENAME_NOREPLACE) == 0)
-        return true;
-
-    // We can also get EINVAL for some non-local filesystems.
-    if (errno != EINVAL) {
-        error = QSystemError(errno, QSystemError::StandardLibraryError);
-        return false;
-    }
-#endif
 #if defined(Q_OS_DARWIN) && defined(RENAME_EXCL)
     if (renameatx_np(AT_FDCWD, srcPath, AT_FDCWD, tgtPath, RENAME_EXCL) == 0)
         return true;
